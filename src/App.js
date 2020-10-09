7578d3c0df5cde621ac1176130d275fb855e3941
Pervez alam <pervezalam777@gmail.com>
2020-10-09 17:12:13 +0530

    linter and prettier configuration added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Title = (props) => <div>{props.text}</div>;
+Title.propTypes = {
+	text: PropTypes.string.isRequired
+};
 
 function App() {
-  return (
-    <div>Boilerplate React, Redux, Redux-Observable</div>
-  );
+	const [title, ] = useState(
+		'Boilerplate React, Redux, Redux-Observable'
+	);
+	return (
+		<div>
+			<Title text={title} />
+		</div>
+	);
 }
 
 export default App;
